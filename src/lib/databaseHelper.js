6be2c56232a3f0e63affd01be25b598f5c7be4cc
Victor Petersen <524091@student.fontys.nl>
2023-04-26 13:48:34 +0200

    update file structure

---
+++ src/lib/databaseHelper.js
@@ -0,0 +1,10 @@
+import { supabase } from "./supabaseClient";
+
+async function fetchData() {
+  const { data, error } = await supabase.from("your_table").select("*");
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+}
