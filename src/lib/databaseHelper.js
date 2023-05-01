e83d5be2af0ba0dff70fc9d2ade704624fc198e0
victorwp <524091@student.fontys.nl>
2023-05-01 12:33:13 +0200

    updated databaseHelper functions

--- src/lib/databaseHelper.js
+++ src/lib/databaseHelper.js
@@ -1,12 +1,39 @@
 import { supabase } from "./supabaseClient";
 
 async function fetchData() {
-  const { data, error } = await supabase.from("your_table").select("*");
+  const { data, error } = await supabase.from("gameData").select("*");
   if (error) {
     console.error(error);
   } else {
     console.log(data);
+	return data;
   }
 }
 
-console.log("test")
+async function insertData(points) {
+	const { error } = await supabase.from("gameData").insert({points})
+	if (error) {
+		console.error(error)
+	} else {
+		console.log("inserted")
+	}
+}
+
+
+async function updateData(id, points) {
+	const { error } = await supabase.from("gameData").update({points: points}).eq("id", id)
+	if (error) {
+		console.error(error)
+	} else {
+		console.log("updated")
+	}
+}
+
+async function deleteData(id) {
+	const { error } = await supabase.from("gameData").delete().eq("id", id)
+	if (error) {
+		console.error(error)
+	} else {
+		console.log("deleted")
+	}
+}
