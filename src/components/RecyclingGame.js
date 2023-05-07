8f1f8b7cc60ba41e4c7f426df5cc41102b108c60
Martin <nnorcco@gmail.com>
2023-05-07 14:09:18 +0200

    backgrounds added, proper lives count, restart fix

--- src/components/RecyclingGame.js
+++ src/components/RecyclingGame.js
@@ -81,9 +81,15 @@ const RecyclingGame = () => {
     }
 
     const backgroundImages = [
-      new Background("background-test.png"),
-      new Background("https://via.placeholder.com/800x600.png?text=Background+2"),
-      new Background("https://via.placeholder.com/800x600.png?text=Background+3"),
+      new Background("background-1.png"),
+      new Background("background-2.png"),
+      new Background("background-3.png"),
+      new Background("background-4.png"),
+      new Background("background-5.png"),
+      new Background("background-6.png"),
+      new Background("background-7.png"),
+      new Background("background-8.png"),
+      // Add more backgrounds here
     ];
 
     let currentBackground = 0;
@@ -125,18 +131,27 @@ const RecyclingGame = () => {
       50
     );
 
+    const itemImages = [
+      "/burger.png",
+      "/cup.png",
+      "/fries.png",
+      "/burger-2.png",
+      "/cup-2.png",
+    ].map((imageSrc) => {
+      const image = new Image();
+      image.src = imageSrc;
+      return image;
+    });
+
     class FallingItem {
-      constructor(x, y, width, height, speed, imageSrc) {
+      constructor(x, y, width, height, speed, image) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.speed = speed;
-        this.image = new Image();
-        this.image.src = imageSrc;
-        this.image.onload = () => {
-          items.push(this);
-        };
+        this.image = image;
+        items.push(this);
       }
 
       draw() {
@@ -149,11 +164,7 @@ const RecyclingGame = () => {
         this.y += this.speed;
         if (this.y + this.height > canvas.height) {
           items.splice(items.indexOf(this), 1);
-          lives--;
-          if (lives <= 0) {
-            clearInterval(updateLoop);
-            alert("Game Over!");
-          }
+          decrementLives();
         }
       }
 
@@ -179,28 +190,16 @@ const RecyclingGame = () => {
       const x = Math.random() * (canvas.width - width);
       const y = 0 - height;
       let speed = 2 + Math.random() * 1 + Math.floor(score / 150) * 0.8;
-      let imageSrc;
+      const image = itemImages[Math.floor(Math.random() * itemImages.length)];
 
-      const randomNum = Math.floor(Math.random() * 5) + 1;
-      switch (randomNum) {
-        case 1:
-          imageSrc = "/burger.png";
-          break;
-        case 2:
-          imageSrc = "/cup.png";
-          break;
-        case 3:
-          imageSrc = "/fries.png";
-          break;
-        case 4:
-          imageSrc = "/burger-2.png";
-          break;
-        case 5:
-          imageSrc = "/cup-2.png";
-          break;
+      const item = new FallingItem(x, y, width, height, speed, image);
+    }
+
+    function decrementLives() {
+      lives--;
+      if (lives <= 0) {
+        resetGame();
       }
-
-      const item = new FallingItem(x, y, width, height, speed, imageSrc);
     }
 
     function update() {
@@ -218,90 +217,83 @@ const RecyclingGame = () => {
           items.splice(i, 1);
           score += 25;
 
-          if (score >= 200 && currentBackground < 1) {
-            currentBackground = 1;
-          } else if (score >= 400 && currentBackground < 2) {
-            currentBackground = 2;
-          }
-        } else if (item.y + item.height > canvas.height) {
-          items.splice(i, 1);
-          lives--;
-          if (lives === 0) {
-            resetGame();
+          if (score % 200 === 0) {
+            currentBackground = (currentBackground + 1) % backgroundImages.length;
           }
         }
       }
     }
 
-      function moveBucket() {
-        if (keys["ArrowLeft"] && bucket.x > 0) {
-          bucket.x -= bucket.speed;
-        }
-  
-        if (keys["ArrowRight"] && bucket.x < canvas.width - bucket.width) {
-          bucket.x += bucket.speed;
-        }
+    function moveBucket() {
+      if (keys["ArrowLeft"] && bucket.x > 0) {
+        bucket.x -= bucket.speed;
       }
-  
-      document.addEventListener("keydown", function (event) {
-        keys[event.code] = true;
-      });
-  
-      document.addEventListener("keyup", function (event) {
-        keys[event.code] = false;
-      });
-  
-      function resetGame() {
-        score = startScore;
-        lives = startLives;
-        items.length = 0;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      if (keys["ArrowRight"] && bucket.x < canvas.width - bucket.width) {
+        bucket.x += bucket.speed;
       }
-  
-      const updateLoop = setInterval(() => {
-        update();
-      }, 10);
-  
-      function startSpawningItems() {
-        if (!spawnInterval) {
-          spawnInterval = setInterval(() => {
-            spawnItem();
-          }, 1000);
-        }
+    }
+
+    document.addEventListener("keydown", function (event) {
+      keys[event.code] = true;
+    });
+
+    document.addEventListener("keyup", function (event) {
+      keys[event.code] = false;
+    });
+
+    function resetGame() {
+      score = startScore;
+      lives = startLives;
+      items.length = 0;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      currentBackground = 0;
+    }
+
+    const updateLoop = setInterval(() => {
+      update();
+    }, 10);
+
+    function startSpawningItems() {
+      if (!spawnInterval) {
+        spawnInterval = setInterval(() => {
+          spawnItem();
+        }, 1000);
       }
-  
-      function stopSpawningItems() {
-        if (spawnInterval) {
-          clearInterval(spawnInterval);
-          spawnInterval = null;
-        }
+    }
+
+    function stopSpawningItems() {
+      if (spawnInterval) {
+        clearInterval(spawnInterval);
+        spawnInterval = null;
       }
-  
-      startSpawningItems();
-      resizeCanvas();
-  
-      function handleVisibilityChange() {
-        if (document.hidden) {
-          stopSpawningItems();
-        } else {
-          startSpawningItems();
-        }
+    }
+
+    startSpawningItems();
+    resizeCanvas();
+
+    function handleVisibilityChange() {
+      if (document.hidden) {
+        stopSpawningItems();
+      } else {
+        startSpawningItems();
       }
-  
-      document.addEventListener("visibilitychange", handleVisibilityChange);
-  
-      window.addEventListener("resize", resizeCanvas);
-  
-      canvas.addEventListener("touchstart", handleTouchStart, false);
-      canvas.addEventListener("touchmove", handleTouchMove, false);
-      canvas.addEventListener("touchend", handleTouchEnd, false);
-    }, []);
-  
-    return (
-      <>
-        <canvas ref={canvasRef} id="canvas" />
-      </>
-    );
-  };
-  
-  export default RecyclingGame;
+    }
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    window.addEventListener("resize", resizeCanvas);
+
+    canvas.addEventListener("touchstart", handleTouchStart, false);
+    canvas.addEventListener("touchmove", handleTouchMove, false);
+    canvas.addEventListener("touchend", handleTouchEnd, false);
+  }, []);
+
+  return (
+    <>
+      <canvas ref={canvasRef} id="canvas" />
+    </>
+  );
+};
+
+export default RecyclingGame;
