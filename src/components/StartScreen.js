868d01659ab14d302d51fe111252acc1b7bbde3a
Martin <nnorcco@gmail.com>
2023-05-20 22:41:30 +0200

    more css styling + css loader for DB table

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -4,15 +4,17 @@ const StartScreen = ({ onStartGame, onLeaderboards, onShop }) => {
   return (
     <div className="startscreen-wrapper">
       <div className="startscreen-container">
-        <button className="startscreen-button" onClick={onStartGame}>
+        <button className="startscreen-button startgame-button" onClick={onStartGame}>
           Start Game
         </button>
-        <button className="startscreen-button" onClick={onLeaderboards}>
-          Leaderboard
-        </button>
-        <button className="startscreen-button" onClick={onShop}>
-          Shop
-        </button>
+        <div className="buttons-row">
+          <button className="startscreen-button leaderboard-button" onClick={onLeaderboards}>
+            Leaderboard
+          </button>
+          <button className="startscreen-button shop-button" onClick={onShop}>
+            Shop
+          </button>
+        </div>
       </div>
     </div>
   );
