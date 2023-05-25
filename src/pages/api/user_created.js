6e26d3eb19c7bb488ca796d543d0ab7a4918ec2d
Victor Petersen <524091@student.fontys.nl>
2023-05-25 12:13:45 +0200

    commented out a file

--- src/pages/api/user_created.js
+++ src/pages/api/user_created.js
@@ -1,22 +1,22 @@
 // pages/api/user_created.js
-import { insertData } from "../../lib/supabaseClient";
-import { supabase } from "../../lib/supabaseClient";
+// import { insertData } from "../../lib/supabaseClient";
+// import { supabase } from "../../lib/supabaseClient";
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { user } = req.body.data.first_name;
+// export default async function handler(req, res) {
+//   if (req.method === "POST") {
+//     const { user } = req.body.data.first_name;
 
-    try {
-      const { error } = await supabase
-        .from("gameData")
-        .insert([{name: user}]);
-      res.status(200).json({ message: "User created successfully." });
-    } catch (error) {
-      res.status(500).json({ error: "Error creating user.", message: error });
-      console.error(error);
-    }
-  } else {
-    res.status(405).json({ error: "Method not allowed." });
-    console.error(error);
-  }
-}
+//     try {
+//       const { error } = await supabase
+//         .from("gameData")
+//         .insert([{name: user}]);
+//       res.status(200).json({ message: "User created successfully." });
+//     } catch (error) {
+//       res.status(500).json({ error: "Error creating user.", message: error });
+//       console.error(error);
+//     }
+//   } else {
+//     res.status(405).json({ error: "Method not allowed." });
+//     console.error(error);
+//   }
+// }
