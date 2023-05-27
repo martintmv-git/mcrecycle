3edaac6e7750bf6b7a9ab818f8b4f4b9ecfce44a
Martin <nnorcco@gmail.com>
2023-05-27 15:50:16 +0200

    unique menu items + id.js and itemdata.js

---
+++ src/pages/itemData.js
@@ -0,0 +1,28 @@
+// itemData.js
+const itemData = [
+    {
+      id: "1",
+      name: "Item 1",
+      imageUrl: "/item3.jpg",
+      description: "This is item 1",
+      price: 100,
+    },
+    {
+      id: "2",
+      name: " McFlurry Vegan Menu",
+      imageUrl: "/item2.jpg",
+      description: "1x McFlurry Vegan Burger | 1x Fries ",
+      price: 200,
+    },
+    {
+      id: "3",
+      name: "Burger & Fries Menu",
+      imageUrl: "/item1.jpg",
+      description: "1x McPlant Burger | 2x Medium Fries",
+      price: 300,
+    },
+    // ... add the rest of items here
+  ];
+  
+  export default itemData;
+  
